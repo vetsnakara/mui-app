7c7ed73cd16eec1f06af2d134d9c76fd9264de9f
Konstantin Arakantsev <kostya3312@gmail.com>
2020-02-19 20:27:00 +0300

    Add svg icons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,66 @@
 import React from "react";
 
-import { Drawer, List, ListItem, ListItemText } from "@material-ui/core";
+import {
+  Drawer,
+  MenuList,
+  MenuItem,
+  ListItemIcon,
+  Typography
+} from "@material-ui/core";
+
+import { green } from "@material-ui/core/colors";
+
+import {
+  Home as HomeIcon,
+  ContactMail as ContactMailIcon
+} from "@material-ui/icons";
+
+import Icon from "@material-ui/core/Icon";
 
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   menuList: {
     minWidth: 200,
-    padding: "20px 10px"
+    padding: "20px 0"
+  },
+  menuItem: {
+    marginBottom: 10
   }
 });
+
+const menuItems = [
+  { title: "Home", icon: HomeIcon },
+  { title: "Contact", icon: ContactMailIcon }
+];
 
 const Menu = ({ open, onClose }) => {
   const classes = useStyles();
 
   return (
     <Drawer open={open} onClose={onClose}>
-      <List className={classes.menuList}>
-        <ListItem>
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="About" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Contacts" />
-        </ListItem>
-      </List>
+      <MenuList className={classes.menuList}>
+        {menuItems.map(({ title, icon: Icon }) => (
+          <MenuItem className={classes.menuItem}>
+            <ListItemIcon>
+              <Icon color="primary" />
+            </ListItemIcon>
+            <Typography variant="body1">{title}</Typography>
+          </MenuItem>
+        ))}
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon>
+            <Icon color="primary">star</Icon>
+          </ListItemIcon>
+          <Typography variant="body1">Favorites</Typography>
+        </MenuItem>
+        <MenuItem className={classes.menuItem}>
+          <ListItemIcon>
+            <Icon className="fa fa-plus-circle" style={{ color: green[500] }} />
+          </ListItemIcon>
+          <Typography variant="body1">Bookmarks</Typography>
+        </MenuItem>
+      </MenuList>
     </Drawer>
   );
 };
